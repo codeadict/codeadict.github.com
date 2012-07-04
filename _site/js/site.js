265463ae7ebf045baa71aee98fb817da8e1d01c8
FourAxis <fouraxis@openerp-fouraxis.(none)>
2012-07-04 14:42:21 -0500

    Migrados posts desde WP.com

---
+++ _site/js/site.js
@@ -0,0 +1,16 @@
+(function($){
+    "use strict";
+
+    $(document).ready(function(){
+    //effect on references
+    $('.testimonials').innerfade({
+        animationtype: 'fade',
+        speed: 'slow',
+        timeout: 6000,
+        type: 'random',
+        containerheight: '135px'
+    });
+    });
+
+})(jQuery);
+
